3ac2e035754148f9527cb1a7a6de6c8cb7a4f5b6
Milan François <milan.francois@etu.minesparis.psl.eu>
2025-01-31 10:51:08 +0100

    global grid

--- Global.py
+++ Global.py
@@ -6,14 +6,29 @@ import pygame as pg
 from pygame.locals import *
 import pygame, sys
 # Initialisation
+
+BLACK = (0, 0, 0)
+WHITE = (200, 200, 200)
+WINDOW_WIDTH=400
+WINDOW_HEIGHT=400
+global SCREEN, CLOCK
 pygame.init()
-screen = pygame.display.set_mode((400, 300))
-pygame.display.set_caption('MiniRogue')
+SCREEN = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+CLOCK = pygame.time.Clock()
+SCREEN.fill(BLACK)
+def drawGrid():
+    blockSize = 20 #Set the size of the grid block
+    for x in range(0, WINDOW_WIDTH, blockSize):
+        for y in range(0, WINDOW_HEIGHT, blockSize):
+            rect = pygame.Rect(x, y, blockSize, blockSize)
+            pygame.draw.rect(SCREEN, WHITE, rect, 1)
+
 #La boucle de jeu principale
 while True:
+    drawGrid()
     sysFont = pygame.font.SysFont("None", 32)
     rendered = sysFont.render('Hello World', 0, (255,100, 100))
-    screen.blit(rendered, (100, 100))
+    SCREEN.blit(rendered, (WINDOW_HEIGHT, WINDOW_HEIGHT))
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
